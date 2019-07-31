f1e3b8403e4d2c28ca9b208a607bd323a1edbf7a
Omid Ayoobe <OAyoobe@spartaglobal.com>
2019-07-31 19:57:22 +0100

    second commit

---
+++ DevopsMuttablePipeline1.3/files/ps_results1.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# Intro Bash Day 3 of DevOps Mutable Pipelines
+# Eng 36 37
+
+### this file is now for a vagrant dev environemnt provisioning
+sudo apt-get update
+sudo apt-get install nginx -y
+
+
+
+
+##echo running $0
+##echo "these are the file in my current directory"  
+##if [$# == 0 ]
+##then
+##	echo enter your name
+##	read name
+	
+##	echo enter age
+##	read age
+
+##fi
+##echo hello $name and i am $age old
+##echo thank you
+
+
+
+##### first way of doing it
+
+#ls -al
+#name=$1
+#age=$2
+#num=$3
+#echo hello cutie, my name is $name, i am $age years old, if you like me 079 me on $num
+
+
+####### second way of doing it
+
+#echo "hi whats your name?"
+#read name
+#echo what is your age?
+#read age
+#echo hi there your name is $name and your $age years old
